06dbdd184dd8a3819d7b3234ca988c14386dfe04
deepanshu2101 <deepanshubhardwaj2101@gmail.com>
2022-11-19 09:08:30 +0530

    React App using Youtube Tutorial

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { SiShowware } from 'react-icons/si';
+import { MdOutlineCancel } from 'racr-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+
+import { links } from '../data/dummy';
+
+const Sidebar = () => {
+    return (
+        <div>Sidebar</div>
+    )
+}
+
+export default Sidebar
